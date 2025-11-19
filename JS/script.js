773974929beffaac37d071dbd04b7cc28a4131b8
Mmabatho76 <ST10460268@rcconnect.edu.za>
2025-11-19 17:57:43 +0000

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -36,4 +36,39 @@ accordionButtons.forEach(btn => {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   
+// ---------------- LIGHTBOX ----------------
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+
+document.querySelectorAll(".popup-img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = "flex";
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+
+// ---------------- BACK TO TOP ----------------
+const topBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  topBtn.style.display = window.scrollY > 300 ? "block" : "none";
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
+document.querySelectorAll(".popup-img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = "flex";
+  });
+});
+
 
